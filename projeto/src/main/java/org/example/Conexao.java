7a27bbfa5c67d3f1f45ea64e75d056025b605857
Matheus Eduardo <matheuslrego7@gmail.com>
2024-11-08 07:45:51 -0300

    fix

--- projeto/src/main/java/org/example/Conexao.java
+++ projeto/src/main/java/org/example/Conexao.java
@@ -1,22 +1,27 @@
-package com.eventos;
+package org.example;
+
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 
 public class Conexao {
-    
+
     private MongoClient cliente;
     private MongoDatabase banco;
-    
+
     public Conexao(){
         cliente = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
         banco = cliente.getDatabase("eventosdb");
-        
+
         System.out.println("Conexão realizada com sucesso!");
     }
 
     public MongoDatabase getBanco() {
         return banco;
-    }        
-    
+    }
+
     public static void main(String[] args) {
         Conexao conn = new Conexao();
     }
 
+}
